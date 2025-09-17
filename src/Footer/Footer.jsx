3ef9fd5178c948ca425737a1fb7bcc031f255b38
opooye123 <amnotpissed@gmail.com>
2025-09-17 22:24:04 +0100

    initial commit

---
+++ src/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+const Footer = () => {
+  const footerStyle = {
+    color: "green",
+    fontStyle: "italic",
+  };
+  return (
+    <div style={footerStyle}>
+      <br />
+      <p>Note app,Department of GehGeh university</p>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ src/main.jsx
@@ -0,0 +1,56 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import axios from "axios";
+import Page from "./Deploying/Form/Page";
+import "./index.css";
+/* import Appl from './App.jsx' */
+/* const noted = axios
+                .get('http://localhost:3001/notes')
+                .then(res => {
+                    const notess = res.data
+                    return notess
+                })
+console.log(noted)
+
+const notes = [noted] */
+
+/* [
+  {
+    id: 1,
+    content: 'HTML is easy',
+    important: true
+  },
+  {
+    id: 2,
+    content: 'Browser can execute only JavaScript',
+    important: false
+  },
+  {
+    id: 3,
+    content: 'GET and POST are the most important methods of HTTP protocol',
+    important: true
+  }
+] */
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    {/* <Appl notes={notes}/> */}
+    {/* <App /> */}
+    {/*  <Apps /> */}
+    <Page />
+    {/* <Index /> */}
+  </StrictMode>
+);
+
+/* const refresh = () => {
+  root.render(
+    <App counter={counter}/>
+  )
+}
+
+setTimeout( () => {
+    refresh()
+    counter += 1
+},2000)
+refresh()
+counter += 1 */
